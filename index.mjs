7da13d8644dcc38417bdaf7b3076bae20d057d8b
Kara Brightwell <kara@153.io>
2022-12-20 08:45:25 +0000

    refactor into cli

--- index.mjs
+++ index.mjs
@@ -2,14 +2,9 @@ import { Client } from '@googlemaps/google-maps-services-js'
 import * as geolib from 'geolib'
 import Sparkline from 'sparkline-svg'
 import fs from 'node:fs/promises'
+import minimist from 'minimist'
 
 const client = new Client({})
-
-const start = { lat: 45.31692799142987, lon: 6.525043787857102}
-const bearing = 130
-const steps = 10
-const spacing = 1000
-const width = 6000
 
 async function slice({ origin, bearing, width, distance }) {
 	const centre = geolib.computeDestinationPoint(
@@ -42,41 +37,57 @@ async function slice({ origin, bearing, width, distance }) {
 	return results
 }
 
-const results = await Promise.all(Array.from({ length: steps }, (_, i) => slice({
-	origin: start,
-	bearing,
-	distance: (i + 1) * spacing,
-	width
-})))
+function getElevationSlices({ origin, bearing, steps, spacing, width }) {
+	return Promise.all(Array.from({ length: steps }, (_, i) => slice({
+		origin,
+		bearing,
+		distance: (i + 1) * spacing,
+		width
+	})))
+}
 
-const lines = results.map(slice => {
-	const line = new Sparkline.default(slice.map(point => point.elevation))
-	return line.d
-})
+function formatSvg(results, { steps }) {
+	const lines = results.map(slice => {
+		const line = new Sparkline.default(slice.map(point => point.elevation))
+		return line.d
+	})
 
-const paths = lines.map((line, i) => `
-	<path
-		style="transform: translateY(${(steps - i - 1) * 100 / steps}%) scaleY(30%)"
-		fill="transparent"
-		stroke="currentColor"
-		strokeWidth="1%"
-		d="${line}" />
-`)
+	const paths = lines.map((line, i) => `
+		<path
+			style="transform: translateY(${(steps - i - 1) * 100 / steps}%) scaleY(30%)"
+			fill="transparent"
+			stroke="currentColor"
+			strokeWidth="1%"
+			d="${line}" />
+	`)
 
-const svg = `<?xml version="1.0" encoding="utf-8"?>
-<svg
-	height="100%"
-	preserveAspectRatio="none"
-	version="1.1"
-	viewBox="0 0 100 100"
-	x="0px"
-	xml:space="preserve"
-	xmlns="http://www.w3.org/2000/svg"
-	xmlns:xlink="http://www.w3.org/1999/xlink"
-	y="0px"
-	width="100%"
->
-	${paths.join('\n')}
-</svg>`
+	return `<?xml version="1.0" encoding="utf-8"?>
+	<svg
+		height="100%"
+		preserveAspectRatio="none"
+		version="1.1"
+		viewBox="0 0 100 100"
+		x="0px"
+		xml:space="preserve"
+		xmlns="http://www.w3.org/2000/svg"
+		xmlns:xlink="http://www.w3.org/1999/xlink"
+		y="0px"
+		width="100%"
+	>
+		${paths.join('\n')}
+	</svg>`
+}
 
-await fs.writeFile('test.svg', svg)
+async function main({ lat, lon, bearing, steps = 10, spacing = 1000, width = 6000, output = 'out.svg' }) {
+	const origin = {
+		lat: parseFloat(lat),
+		lon: parseFloat(lon),
+	}
+
+	const data = await getElevationSlices({ origin, bearing: parseFloat(bearing), steps, spacing, width })
+	const svg = formatSvg(data, {steps})
+
+	await fs.writeFile(output, svg)
+}
+
+await main(minimist(process.argv.slice(2)))
